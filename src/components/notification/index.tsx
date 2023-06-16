868f31e46c1984a9e8212acc74467f2dae54ed7a
Phichayoot <54633251+O-RGB@users.noreply.github.com>
2023-06-16 06:13:26 +0000

    aweh

--- src/components/notification/index.tsx
+++ src/components/notification/index.tsx
@@ -13,10 +13,10 @@ const NotificationComponent: React.FC<NotificationComponentProps> = ({
 }) => {
   return (
     <>
-      <div className="w-full bg-white bg-opacity-20 rounded-3xl h-20">
-        <div className="flex justify-between gap-1 w-full h-full">
-          <div className="p-3 h-full  overflow-hidden w-[6.5rem] ">
-            <div className=" h-full bg-white rounded-2xl">
+      <div className="w-full bg-white bg-opacity-20 rounded-3xl h-[5rem]">
+        <div className="flex  justify-between gap-1 w-full h-full">
+          <div className="flex p-3 h-full  overflow-hidden w-[6rem]">
+            <div className="w-full h-full bg-white rounded-2xl">
               <div className="flex justify-center items-center h-full text-2xl">
                 {appIcon}
               </div>
--- src/group/mainPange.tsx
+++ src/group/mainPange.tsx
@@ -22,7 +22,7 @@ const MainPage: React.FC<MainPageProps> = ({}) => {
             <div
               className={`flex flex-col gap-2 pt-4 transition-all duration-1000 ${
                 notification
-                  ? "h-[200px] opacity-100"
+                  ? "h-[17rem] opacity-100"
                   : "h-[1px] overflow-hidden opacity-0"
               } `}
             >
@@ -30,6 +30,11 @@ const MainPage: React.FC<MainPageProps> = ({}) => {
                 appIcon={"🟣"}
                 appName="Fackbook"
                 appDetail="Lorem Ipsum is simply dummy text of"
+              ></NotificationComponent>
+              <NotificationComponent
+                appIcon={"🟣"}
+                appName="VsCode"
+                appDetail="Since it looks like you're trying to make an accordion, I'd"
               ></NotificationComponent>
               <NotificationComponent
                 appIcon={"🟣"}
